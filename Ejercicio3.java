7fd8b561f6d3ee3ac9f2eeab42b691440199001d
JamesCordova <jcordovas@unsa.edu.pe>
2023-05-20 02:41:47 -0500

    Ejercicio3 agregado y el método trianguloRecursivo

---
+++ Ejercicio3.java
@@ -0,0 +1,8 @@
+public class Ejercicio3{
+  public static void main(String[] args){
+    int base = 5;
+    trianguloRecursivo(base);
+  }
+  public static void trianguloRecursivo(int base){
+  }
+}
