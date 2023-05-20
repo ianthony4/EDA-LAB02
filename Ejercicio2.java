82b7c567f5692fa835700093e48f2045a05c93c3
Anthony Chaisa <achaisa@unsa.edu.pe>
2023-05-20 00:32:13 -0500

    Creando el modelo de la clase y la funcion del ejercicio

--- Ejercicio2.java
+++ Ejercicio2.java
@@ -1,1 +1,5 @@
-//Code
+package LAB02;
+public class Ejercicio2{
+    //Main (opcional debido a que el problema no indica colocarlo)
+    //Creamos la funcion que rotara a la izquierda el arreglo
+}
