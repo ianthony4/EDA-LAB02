fabe00681f3afc21d4eb122678181f408598e6e5
Anthony Chaisa <achaisa@unsa.edu.pe>
2023-05-20 00:41:53 -0500

    Primer planteamiento

--- Ejercicio2.java
+++ Ejercicio2.java
@@ -3,13 +3,18 @@ public class Ejercicio2{
     //Main (opcional debido a que el problema no indica colocarlo)
     //Creamos la funcion que rotara a la izquierda el arreglo
     public int[] rotarIzquierdaArray(int[] A){
-        int[] aiz = new int[A.length];
+        int[] Aiz = new int[A.length];
         //Modificable
         int d = 2;
         int aux;
         //for que recorra el arreglo A
         for(int i = 0; i < A.length ; i++){
-            
+            aux = A[i];
+            if(i - d >= 0){
+                //Debemos colocarlo al inicio
+            }else{
+                //Lo colocamos al final
+            }
         }
         return A;
     }
