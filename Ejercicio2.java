6ede6ba6ad68ddac6148dac3d4da0dacdb9324f6
Anthony Chaisa <achaisa@unsa.edu.pe>
2023-05-20 00:34:26 -0500

    Creando el arreglo que sera el resultado final 'AIZ'

--- Ejercicio2.java
+++ Ejercicio2.java
@@ -3,6 +3,7 @@ public class Ejercicio2{
     //Main (opcional debido a que el problema no indica colocarlo)
     //Creamos la funcion que rotara a la izquierda el arreglo
     public int[] rotarIzquierdaArray(int[] A){
+        int[] aiz = new int[A.length];
         return A;
     }
 }
