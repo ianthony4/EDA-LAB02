ccab114b240e69c7131d4e4d320c872ab8dd3b6e
Anthony Chaisa <achaisa@unsa.edu.pe>
2023-05-20 00:33:37 -0500

    Creando la funcion POR AHORA SOLO RETORNARA EL ARREGLO INICIAL PARA EVITAR LOS ERRORES

--- Ejercicio2.java
+++ Ejercicio2.java
@@ -2,4 +2,7 @@ package LAB02;
 public class Ejercicio2{
     //Main (opcional debido a que el problema no indica colocarlo)
     //Creamos la funcion que rotara a la izquierda el arreglo
+    public int[] rotarIzquierdaArray(int[] A){
+        return A;
+    }
 }
