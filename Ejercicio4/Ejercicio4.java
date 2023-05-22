94ea66a2ddb1acedc60ce7f53f72fc8f42105043
Anthony Chaisa <achaisa@unsa.edu.pe>
2023-05-21 20:31:24 -0500

    Añadiendo numeros a nuestra lista de numeros

--- Ejercicio4/Ejercicio4.java
+++ Ejercicio4/Ejercicio4.java
@@ -5,5 +5,8 @@ public class Ejercicio4{
     public static void main(String[] args){
         System.out.println("--- Primera Parte---");
         List<Integer> numbers = new ArrayList<Integer>();
+        //Probaremos metodos de 'List'
+        numbers.add(1);
+        numbers.add(2);
     }
 }
