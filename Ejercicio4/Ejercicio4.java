eeacac98d8a10375b0e7a67722e8a9ce87f97cb6
Anthony Chaisa <achaisa@unsa.edu.pe>
2023-05-21 20:29:57 -0500

    Creando una lista de numeros, importando java.util.

---
+++ Ejercicio4/Ejercicio4.java
@@ -0,0 +1,9 @@
+package LAB02.Ejercicio4;
+import java.util.*;
+
+public class Ejercicio4{
+    public static void main(String[] args){
+        System.out.println("--- Primera Parte---");
+        List<Integer> numbers = new ArrayList<Integer>();
+    }
+}
